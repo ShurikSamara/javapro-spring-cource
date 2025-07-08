f24927281e347b0c0309c9a00883d6412629d6e6
Александр Самаркин <aysamarkin@sberbank.ru>
2025-07-08 11:13:49 +0300

    comments corrected

--- homework-spring-3/src/main/java/ru/learning/java/spring/exception/UserConstraintViolationException.java
+++ homework-spring-3/src/main/java/ru/learning/java/spring/exception/UserConstraintViolationException.java
@@ -1,10 +1,6 @@
 package ru.learning.java.spring.exception;
 
 public class UserConstraintViolationException extends RuntimeException {
-  public UserConstraintViolationException(String message) {
-    super(message);
-  }
-
   public UserConstraintViolationException(String message, Throwable cause) {
     super(message, cause);
   }
