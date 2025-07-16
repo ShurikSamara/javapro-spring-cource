22297e57396643ab8d6feaf83976cc5bcc897d14
Александр Самаркин <aysamarkin@sberbank.ru>
2025-07-16 18:13:39 +0300

    corrected class

--- homework-spring-5/src/main/java/ru/learning/java/spring/config/LimitsServiceProperties.java
+++ homework-spring-5/src/main/java/ru/learning/java/spring/config/LimitsServiceProperties.java
@@ -1,35 +1,36 @@
 package ru.learning.java.spring.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.math.BigDecimal;
 
 /**
  * Класс конфигурации
  */
-@Configuration
+
+@ConfigurationProperties(prefix = "limits-service")
 public class LimitsServiceProperties {
 
-    @Value("${limits-service.default-limit}")
-    private BigDecimal defaultLimit;
+  @Value("${limits-service.default-limit}")
+  private BigDecimal defaultLimit;
 
-    @Value("${limits-service.reset-cron}")
-    private String resetCron;
+  @Value("${limits-service.reset-cron}")
+  private String resetCron;
 
-    public BigDecimal getDefaultLimit() {
-        return defaultLimit;
-    }
+  public BigDecimal getDefaultLimit() {
+    return defaultLimit;
+  }
 
-    public void setDefaultLimit(BigDecimal defaultLimit) {
-        this.defaultLimit = defaultLimit;
-    }
+  public void setDefaultLimit(BigDecimal defaultLimit) {
+    this.defaultLimit = defaultLimit;
+  }
 
-    public String getResetCron() {
-        return resetCron;
-    }
+  public String getResetCron() {
+    return resetCron;
+  }
 
-    public void setResetCron(String resetCron) {
-        this.resetCron = resetCron;
-    }
+  public void setResetCron(String resetCron) {
+    this.resetCron = resetCron;
+  }
 }
