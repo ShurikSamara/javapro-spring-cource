50d02237f6d933ad989304a828b2f8a03fd38573
Александр Самаркин <aysamarkin@sberbank.ru>
2025-07-17 13:49:12 +0300

    delete unused import

--- homework-spring-4/payment-service/src/main/java/ru/learning/java/spring/repository/PaymentRepository.java
+++ homework-spring-4/payment-service/src/main/java/ru/learning/java/spring/repository/PaymentRepository.java
@@ -1,14 +1,9 @@
 package ru.learning.java.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.learning.java.spring.model.Payment;
-import ru.learning.java.spring.model.PaymentStatus;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
