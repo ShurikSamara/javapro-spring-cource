0af04ca8cfa83fb382d44b52a74e9e8f77e862d1
Александр Самаркин <aysamarkin@sberbank.ru>
2025-07-17 22:46:01 +0300

    refactor: delete unused import

--- homework-spring-4/payment-service/src/main/java/ru/learning/java/spring/service/ProductIntegrationService.java
+++ homework-spring-4/payment-service/src/main/java/ru/learning/java/spring/service/ProductIntegrationService.java
@@ -6,7 +6,6 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
