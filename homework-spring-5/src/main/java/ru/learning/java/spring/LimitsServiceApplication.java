599bde46beaffd34534c93cf2172afb34b5de0b1
Александр Самаркин <aysamarkin@sberbank.ru>
2025-07-18 11:25:38 +0300

    refactor: delete logger and @EnableConfigurationProperties

--- homework-spring-5/src/main/java/ru/learning/java/spring/LimitsServiceApplication.java
+++ homework-spring-5/src/main/java/ru/learning/java/spring/LimitsServiceApplication.java
@@ -1,22 +1,12 @@
 package ru.learning.java.spring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.learning.java.spring.config.LimitsServiceProperties;
-
 
 @SpringBootApplication
-@EnableConfigurationProperties(LimitsServiceProperties.class)
 @EnableScheduling
 public class LimitsServiceApplication {
-    private static final Logger log = LoggerFactory.getLogger(LimitsServiceApplication.class);
-
-    public static void main(String[] args) {
-        log.info("Starting Limits Service Application");
-        SpringApplication.run(LimitsServiceApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(LimitsServiceApplication.class, args);
     }
 }
